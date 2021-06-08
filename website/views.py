d2753d88934a398d494ccfd68c11584848b0db8b
omkarhcode <omkarh.code@gmail.com>
2021-06-09 00:05:01 +0530

    Update contact from section

--- website/views.py
+++ website/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
+
+	if request.method == "POST":
+		your_name = request.POST['your-name']
+		your_email = request.POST['your-email']
+		your_subject = request.POST['your-subject']
+		your_message = request.POST['your-message']
+		return render(request, 'home.html' ,{'your_name': your_name})
+	
 	return render(request, 'home.html', {})
