0b4c989e5ddefc87808124cb7addbd772742406f
omkarhcode <omkarh.code@gmail.com>
2021-06-09 09:35:55 +0530

    Added django send_mail for contact form

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,3 +132,26 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Watch this - https://youtu.be/xNqnHmXIuzU
+# Gmail will only work if you have allowed "Less secure apps" and display unlock captcha 
+# https://myaccount.google.com/lesssecureapps
+# https://accounts.google.com/b/0/DisplayUnlockCaptcha
+
+# OR use App password which needs 2 factor authentication. - https://support.google.com/accounts/answer/185833?hl=en
+
+# Email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'omkarhcode@gmail.com'
+EMAIL_HOST_PASSWORD = '******'
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False 
+
+
+
+
+
+
--- website/views.py
+++ website/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 
 def home(request):
 
+	# Get data from contact from on home page
 	if request.method == "POST":
 		your_name = request.POST['your-name']
 		your_email = request.POST['your-email']
 		your_subject = request.POST['your-subject']
 		your_message = request.POST['your-message']
+
+		# Send an E-mail
+		# send_mail(subject, message, from_email, recipient_list)
+		send_mail(your_name + your_subject, your_message, your_email, ['omkarh.work@gmail.com', 'omkarh@gmail.com'],)
+
 		return render(request, 'home.html' ,{'your_name': your_name})
-	
-	return render(request, 'home.html', {})
+	else:
+		return render(request, 'home.html', {})
